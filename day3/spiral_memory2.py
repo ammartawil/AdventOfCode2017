843e4c77ce079ff4c945b398db20e5888e154ae1
Ammar Al-Taweel <ammaraltaweel@Ammars-MacBook-Pro.local>
2018-01-02 09:52:42 +0000

    Add better formatting for output
    Also reorganise the code.

--- day3/spiral_memory2.py
+++ day3/spiral_memory2.py
@@ -2,29 +2,12 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# Build a list of lists to hold the spiral
-
-spiral = []
-spiral_size = 11
-row = []
-for i in range(0, spiral_size):
-    row.append(0)
-
-for column in range(0,spiral_size):
-    spiral.append([0]*spiral_size)
-
-
-offset = int(spiral_size/2)
-spiral[offset][offset]=1
-
-# Build the spiral
 
 def calc_new_value(x,y):
-# Calculates the value of position spiral[x][y] by adding all the cells around it.
+    # Calculates the value of position spiral[x][y] by adding all the cells around it.
     value=0
     for xcell in range(offset+x-1, offset+x+2):
         for ycell in range(offset+y-1, offset+y+2):
-            # print('adding cell[{}][{}].{}'.format(xcell,ycell,spiral[xcell][ycell]))
             value += spiral[xcell][ycell]
     return value
 
@@ -35,6 +18,20 @@ def set_cell_value(x,y,v):
     spiral[x+offset][y+offset] = v
 
 
+# Build a list of lists to hold the spiral
+spiral = []
+spiral_size = 11
+row = []
+for i in range(0, spiral_size):
+    row.append(0)
+
+for column in range(0,spiral_size):
+    spiral.append([0]*spiral_size)
+
+offset = int(spiral_size/2)
+spiral[offset][offset]=1
+
+# Build the spiral
 steps = 2
 current_x = 0
 current_y = 0
@@ -64,7 +61,9 @@ while get_cell_value(current_x,current_y) <= 361527:
 
     steps += 2
 
-for i in range(spiral_size-1,0,-1):
-    print(spiral[i].)
+for row in spiral:
+    for n in row:
+        print('{0:07}'.format(n), end='  ')
+    print('\n')
 
 # Answer = 363010
